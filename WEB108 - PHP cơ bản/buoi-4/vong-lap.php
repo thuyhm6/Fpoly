f06fd26f493066f87dd275c4bf6bce3b77026c5a
thuyhm6 <149662392+thuyhm6@users.noreply.github.com>
2024-03-13 20:58:39 +0700

    Update vong-lap.php

--- WEB108 - PHP cơ bản/buoi-4/vong-lap.php
+++ WEB108 - PHP cơ bản/buoi-4/vong-lap.php
@@ -1,33 +1,33 @@
 <?php 
      $a = 1;
      //In ra bảng cửu chương với vòng lặp while
-    /*  while ($a < 10) {
+    while ($a < 10) {
          echo " 2 x $a = ".($a*2)." <br/>";
          $a++;
-     } */
+     }
      //Vong lặp do while
-     /* do {
+     do {
         echo "biến a là $a <br/>";
         $a++;
-     } while ($a < 10); */
+     } while ($a < 10);
 
      //Vòng lặp for
-     /* for ($i = 0; $i < 10; $i++) {
+     for ($i = 0; $i < 10; $i++) {
         echo "Biến $i <br/>";
      };
-*/
-     /* for ($e = 1; $e <= 10; $e++):
+
+     for ($e = 1; $e <= 10; $e++):
         
          if ($e === 5) {
             echo "Đây là $e<br/>";
             continue;
          }
          echo "4 x $e = ".(4*$e)." <br/>";
-     endfor;  */
+     endfor;
 
      //Vòng lặp foreach
 
-     /* $arr = array(1,2,3,4,5);
+     $arr = array(1,2,3,4,5);
      print_r($arr);
      echo "$arr[3]";
      for ($i = 0; $i < count($arr); $i++):
@@ -38,16 +38,16 @@
         'ten' => 'Thủy',
         'tuoi' => 17,
         'dia_chi' => 'Thanh Hóa'
-     ]; */
+     ];
      //Cách lấy ra value bằng foreach
-     /* foreach ($assArr as $val) {
+     foreach ($assArr as $val) {
         echo "$val <br/>";
-     } */
+     }
 
      //Cách lấy ra cả key và value bằng foreach
-     /* foreach ($assArr as $k => $val):
+     foreach ($assArr as $k => $val):
         echo "$k = $val <br/>";
-     endforeach; */
+     endforeach;
      lap:
      if ($a < 10) {
         echo " 2 x $a = ".($a*2)." <br/>";
