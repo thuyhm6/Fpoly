8a430a9f0679b961f9cca6db58bd1063ca3b7d3a
thuyhm6 <149662392+thuyhm6@users.noreply.github.com>
2024-05-31 09:33:10 +0700

    Add files via upload

---
+++ WEB2014-PHP 1/buoi-12-CRUD/commons/baseModel.php
@@ -0,0 +1,54 @@
+<?php 
+    namespace commons;
+
+    use PDO;
+    
+    class baseModel {
+        public $conn;
+        public function __construct()
+        {
+            $db_host = DB_HOST;
+            $db_name = DB_NAME;
+
+            $this->conn = new PDO("mysql:host=$db_host;dbname=$db_name", DB_USER, DB_PASS);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        public function getAll($tableName) {
+            
+            $sql = "SELECT * FROM $tableName";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }
+
+        public function delAll($tableName, $id) {
+            $sql = "DELETE FROM $tableName where ma_san_pham = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$id]);
+            //return $stmt->fetchAll();
+        }
+
+        public function find($tableName, $id) {            
+            $sql = "SELECT * FROM $tableName where ma_san_pham = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$id]);
+            return $stmt->fetch();
+        }
+
+        public function updateByID($tableName, $id, $data) {
+            $tenSanPham = $data['ten_san_pham'];
+            $gia = $data['gia'];
+            //Thầy để sẵn cái gợi ý ở đây để a e triển khai tiếp xem nhé
+            $colums = [];
+            foreach ($data as $key => $value) {
+                $colums[] = "$key= :$key";
+            }
+            debug(implode(",", $colums));
+            $sql = "UPDATE $tableName SET ".implode(",", $colums)." WHERE MA_SAN_PHAM = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([$tenSanPham, $gia, $id]);
+        }
+
+        
+    }
+?>
---
+++ WEB2014-PHP 1/buoi-12-CRUD/header.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/php1/buoi-12-CRUD/asset/css/style.css">
+    <title>Document</title>
+</head>
