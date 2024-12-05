10e4c95ce1081f8612efe0cfa53023f985939214
thuyhm6 <149662392+thuyhm6@users.noreply.github.com>
2024-12-05 23:17:39 +0700

    Update main.js

--- thi-thu-js-nang-cao/main.js
+++ thi-thu-js-nang-cao/main.js
@@ -22,11 +22,12 @@ function loadProduct() {
     document.querySelector('#product-secsion').style.display = 'block';
     const productListHTML = document.querySelector('.product-list');
     fetch(url+'product').then((res) => res.json()).then((products) => {
+        // console.log(products.category);
         const productList = products.map((product) => {
-            return `<tr>
+            return `<tr class="product-data-${product.id}">
                     <td>${product.category}</td>
                     <td>${product.prodName}</td>
-                    <td><button class="btn btn-danger" onclick="deleteProduct('${product.id}')">Xóa</button>
+                    <td><button class="btn btn-warning" onclick="editProduct('${product.id}')">Sửa</button><button class="btn btn-danger" onclick="deleteProduct('${product.id}')">Xóa</button>
 </td>
                 </tr>`
         });
@@ -35,4 +36,49 @@ function loadProduct() {
     })
 }
 
-// loadProduct();
+loadProduct();
+
+function deleteProduct(id){
+    if (confirm('Bạn chắc chắn xóa không')) {
+        //Đi xóa
+        fetch(url+'product/'+id, {method: 'DELETE'}).then(() => {
+            //alert('Xóa thành công');
+            //loadProduct();
+            const productData = document.querySelector('.product-data-'+id);
+            productData.remove();
+        });
+    }
+}
+
+document.querySelector('.product-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const category = document.querySelector('#category').value;
+    const prodName = document.querySelector('#product-name').value;
+    const ProductFormData = {category, prodName};
+    
+    //thằng này để thêm
+    const postOption = {method: 'POST',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify(ProductFormData)
+    };
+    //thằng này để sửa
+    const putOption = {method: 'PUT',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify(ProductFormData)
+    };
+
+    fetch(url+'product', postOption).then(() => {
+        alert('Thêm thành công');
+        loadProduct();
+        category = '';
+    })
+})
+
+function editProduct(id){
+    console.log(id);
+    fetch(url+'product/'+id).then((res) => res.json()).then((products) => {
+        document.querySelector("#category").value = products.category;
+        document.querySelector("#productID").value = id;
+        
+    })
+}
